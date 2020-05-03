0e22ae2a5b854404fa4ee31a6ad2ee8f38ff9989
yunhong-0512 <56242772+yunhong-0512@users.noreply.github.com>
2020-05-03 09:17:58 +0800

    version 1

--- proj0/Body.java
+++ proj0/Body.java
@@ -1,101 +1,82 @@
-public class Body{
-/** @param
-    xxPos: current x postion
-    yyPos: current y positon
-    xxVel: current velocity in the x direction
-    yyVel: current velocity in the y direction
-    mass: mass
-    imgFileName: The name of the file that corresponds to the image that depicts the body
-*/
-  public double xxPos;
-  public double yyPos;
-  public double xxVel;
-  public double yyVel;
-  public double mass;
-  public String imgFileName;
-
-  public double G = 6.67e-11;
-
-  public Body(double xP, double yP, double xV,
-              double yV, double m, String img){
-    xxPos = xP;
-    yyPos = yP;
-    xxVel = xV;
-    yyVel = yV;
-    mass = m;
-    imgFileName = img;
-              }
-
-  public Body(Body b){
-    xxPos = b.xxPos;
-    yyPos = b.yyPos;
-    xxVel = b.xxVel;
-    yyVel = b.yyVel;
-    mass = b.mass;
-    imgFileName = b.imgFileName;  }
-
-    // calculete the distance
-    public double calcDistance(Body b){
-      double distance = Math.sqrt(
-                        Math.pow((xxPos - b.xxPos), 2) + Math.pow((yyPos - b.yyPos), 2));
-      return distance;
+public class Body {
+	public double xxPos;   // Its current x position
+	public double yyPos;   // Its current y position
+	public double xxVel;   // Its current velocity in the x direction
+	public double yyVel;   // Its current velocity in the y direction
+	public double mass;    // Its mass
+	public String imgFileName; //The name of the file that corresponds to the image that depicts the body
+    public static double G = 6.67E-11;   //gravitational constant
+    /** First constructer */
+    public Body(double xP, double yP, double xV, double yV, double m, String img) {
+    	xxPos = xP;
+    	yyPos = yP;
+    	xxVel = xV;
+    	yyVel = yV;
+    	mass = m;
+    	imgFileName = img;
     }
 
-    // calcalute the force
-    public double calcForceExertedBy(Body b){
-      double force = G * mass * b.mass / Math.pow(calcDistance(b), 2);
-      return force;
+    /** Sencond constructer
+     *  take in a Body object and initialize an identical Body object
+     */
+    public Body(Body b) {
+    	xxPos = b.xxPos;
+    	yyPos = b.yyPos;
+    	xxVel = b.xxVel;
+    	yyVel = b.yyVel;
+    	mass = b.mass;
+    	imgFileName = b.imgFileName;
     }
 
-    //calculate the force in X direction
-    public double calcForceExertedByX(Body b){
-      double xforce = calcForceExertedBy(b) * ((b.xxPos - xxPos) / calcDistance(b));
-      return xforce;
+    /** Return the distance between two Bodys */
+    public double calcDistance(Body b) {
+    	return Math.sqrt(Math.pow(b.xxPos - this.xxPos, 2) + Math.pow(b.yyPos - this.yyPos, 2));
     }
 
-    //calculate the force in Y direction
-    public double calcForceExertedByY(Body b){
-      double yforce = calcForceExertedBy(b) * ((b.yyPos - yyPos) / calcDistance(b));
-      return yforce;
+    /** Return a double describing the force exerted on this body by the given body. */
+    public double calcForceExertedBy(Body b) {
+    	return (G * b.mass * this.mass) / Math.pow(this.calcDistance(b), 2);
     }
 
-    //calculate the net force in X direction
-    public double calcNetForceExertedByX(Body[] net){
-      double xNetForce = 0;
-      for (int i = 0; i < net.length; i++){
-        if (!(this.equals(net[i]))){
-          xNetForce += calcForceExertedByX(net[i]);
-        }
-      }
-      return xNetForce;
+    /** Return the force exerted in the X direction */
+    public double calcForceExertedByX(Body b) {
+    	return this.calcForceExertedBy(b) * (b.xxPos - this.xxPos) / this.calcDistance(b);
     }
 
-    //calculate the net force in Y direction
-    public double calcNetForceExertedByY(Body[] net){
-      double yNetForce = 0;
-      for (int i = 0; i < net.length; i++){
-        if (!(this.equals(net[i]))){
-        yNetForce += calcForceExertedByY(net[i]);
-        }
-      }
-      return yNetForce;
+    /** Return the force exerted in the Y direction */
+    public double calcForceExertedByY(Body b) {
+    	return this.calcForceExertedBy(b) * (b.yyPos - this.yyPos) / this.calcDistance(b);
     }
 
-    //update the velocity and the postion
-    public void update(double dt, double fX, double fY){
-      double aX = fX / mass;
-      double aY = fY / mass;
-      xxVel += dt * aX;
-      yyVel += dt * aY;
-      xxPos += dt * xxVel;
-      yyPos += dt * yyVel;
+    /** Return the net X force exerted by all bodies upon the current Body. */
+    public double calcNetForceExertedByX(Body[] allBodys) {
+    	double netForce = 0;
+    	for (Body b : allBodys) {
+    		if (!b.equals(this)) {
+    			netForce += this.calcForceExertedByX(b);
+    		}
+    	}
+    	return netForce;
     }
 
-    //draw bodys
-    public void draw(){
-      StdDraw.picture(xxPos, yyPos, "./images/"+imgFileName);
+    /** Return the net Y force exerted by all bodies upon the current Body. */
+    public double calcNetForceExertedByY(Body[] allBodys) {
+    	double netForce = 0;
+    	for (Body b : allBodys) {
+    		if (!b.equals(this)) {
+    			netForce += this.calcForceExertedByY(b);
+    		}
+    	}
+    	return netForce;
     }
-
-
-
+    
+    /** Update the postion and velcity */
+    public void update(double dt, double fX, double fY) {
+    	double xA = fX / mass;
+    	double yA = fY / mass;
+    	xxVel += xA * dt;
+    	yyVel += yA * dt;
+    	xxPos += xxVel * dt;
+    	yyPos += yyVel * dt;
+    }
 }
--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,87 +1,81 @@
 public class NBody {
 
-  //filelength defines how many objects in the universe
-  public int filelength = 5;
+	/** Return the radius of the universe in the given file */
+	public static double readRadius(String filename) {
+		In in = new In(filename);
+		in.readDouble();
+		double radius = in.readDouble();
+		return radius;
+	}
 
-  //return the radius of the universe
-  public static double readRadius(String file) {
-    In in = new In(file);
-    int num = in.readInt();
-    double radius = in.readDouble();
-    return radius;
-  }
+	/** Return an array of Bodys corresponding to the bodies in the given file */
+	public static Body[] readBodies(String filename) {
+		In in = new In(filename);
+		int N = in.readInt();
+		in.readDouble();
+		Body[] universe = new Body[N];
+		for (int i = 0; i < N; i++) {
+			double xP = in.readDouble();
+			double yP = in.readDouble();
+			double xV = in.readDouble();
+			double yV = in.readDouble();
+			double m = in.readDouble();
+			String img = in.readString();
+			universe[i] = new Body(xP, yP, xV, yV, m, img);
+		}
+		return universe;
+	}
 
-  //return the univese Body
-  public static Body[] readBodies(String file) {
-    In in = new In(file);
-    Body[] planets = new Body[5];
-    in.readInt();
-    in.readDouble();
-    for (int i = 0; i < 5; i++){
-      double xxPos = in.readDouble();
-      double yyPos = in.readDouble();
-      double xxVel = in.readDouble();
-      double yyVel = in.readDouble();
-      double mass = in.readDouble();
-      String imgFileName = in.readString();
-      planets[i] = new Body(xxPos, yyPos, xxVel, yyVel, mass, imgFileName);
-    }
-    return planets;
-  }
+	public static void main(String[] args) {
+		// cllect all needed input
+		double T = Double.parseDouble(args[0]);
+		double dt = Double.parseDouble(args[1]);
+		String filename = args[2];
+		double radius = readRadius(filename);
+		Body[] bodies = readBodies(filename);
 
+		//draw the background
+		String imageToDraw = "images/starfield.jpg";
+		StdDraw.enableDoubleBuffering();
+		int scale = 100; //Sets up the universe
+		StdDraw.setScale(-1 * scale, scale);
+		StdDraw.clear();
 
-  public static void  main(String[] args) {
+        // Anination
+        double time = 0;
+        while (time < T) {
 
-    //collecting inputs
-    double T = Double.parseDouble(args[0]);
-    double dt = Double.parseDouble(args[1]);
-    String filename = args[2];
-    double radius = readRadius(filename);
-    Body[] planets = readBodies(filename);
+        	double[] xForces = new double[bodies.length];
+        	double[] yForces = new double[bodies.length];
+        	// Calculate the net x and y forces for each Body
+        	for (int i = 0; i < bodies.length; i++) {
+        		xForces[i] = bodies[i].calcNetForceExertedByX(bodies);
+        		yForces[i] = bodies[i].calcNetForceExertedByY(bodies);
+        	}
+        	// Draw the background image
+        	StdDraw.picture(0, 0, imageToDraw);
+        	// Draw all of the Bodies
+        	for (int i = 0; i < bodies.length; i++) {
+        		// update each body’s position, velocity, and acceleration
+        		bodies[i].update(dt, xForces[i], yForces[i]);
+        	    StdDraw.picture(bodies[i].xxPos / radius * scale, bodies[i].yyPos / radius * scale, "images/" + bodies[i].imgFileName);
+            }
+            // Show the offscreen buffer
+            StdDraw.show();
+            // Pause the animation for 10 milliseconds
+            StdDraw.pause(10);
+            //Increase time variable
+            time += dt;
+        }
 
-    //draw the background
-    String background = "./images/starfield.jpg";
-    StdDraw.clear();
-    StdDraw.setScale(-radius, radius);
-    StdDraw.picture(0, 0, background, 2*radius , 2*radius);
-    StdDraw.show();
+        // print out the final state of the universe
+        StdOut.printf("%d\n", bodies.length);
+        StdOut.printf("%.2e\n", radius);
+        for (int i = 0; i < bodies.length; i++) {
+            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+                          bodies[i].xxPos, bodies[i].yyPos, bodies[i].xxVel,
+                          bodies[i].yyVel, bodies[i].mass, bodies[i].imgFileName);   
+        }
 
-    //draw planets
-    for (int i = 0; i < planets.length ; i++){
-      planets[i].draw();
-    }
-
-    StdDraw.enableDoubleBuffering();
-
-    // for each enterval st, update the postion of the planets and show them
-    for (int t = 0; t <= T; t += dt){
-      double[] xForce = new double[5];
-      double[] yForce = new double[5];
-      for (int j = 0; j < planets.length; j++){
-        xForce[j] = planets[j].calcNetForceExertedByX(planets);
-        yForce[j] = planets[j].calcNetForceExertedByY(planets);
-        planets[j].update(dt, xForce[j], yForce[j]);
-      }
-
-      //draw background
-      StdDraw.picture(0, 0, background, 2*radius , 2*radius);
-
-      //draw all planets
-      for (int i = 0; i < planets.length ; i++){
-        planets[i].draw();
-      }
-      StdDraw.show();
-      StdDraw.pause(10);
-    }
-
-    StdOut.printf("%d\n", planets.length);
-    StdOut.printf("%.2e\n", radius);
-    for (int i = 0; i < planets.length; i++) {
-        StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
-                      planets[i].xxPos, planets[i].yyPos, planets[i].xxVel,
-                      planets[i].yyVel, planets[i].mass, planets[i].imgFileName);
-    }
-  }
-
-
+	}
 }
